3f42b3b270f6c867ee992ca32293a74d0015deca
RodrigoVon <rodrigo.von.ing@gmail.com>
2023-08-24 23:11:19 -0400

    feat: se resuelve ejercicio de sumar

---
+++ Ejercicios/suma.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+puts 'Ingresa el primer número:'
+numero_uno = gets.chomp.to_i
+puts 'Ingresa el segundo número:'
+numero_dos = gets.chomp.to_i
+puts "El resultado es: #{numero_uno + numero_dos}"
